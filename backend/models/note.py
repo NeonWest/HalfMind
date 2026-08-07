6dda428b2e28fa3b248b49e1555dfea302f88e95
Nihat-Hcyv <haciyevnihat73@gmail.com>
2026-08-07 21:42:21 +0400

    created models

---
+++ backend/models/note.py
@@ -0,0 +1,8 @@
+class Note:
+    def __init__(self, userid, title, body, createdAt):
+        self.userid = userid
+        self.title = title
+        self.body = body
+        self.createdAt = createdAt
+
+    
---
+++ backend/models/user.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, userid, username, email, password, createdAt):
+        self.userid = userid
+        self.username = username
+        self.email = email
+        self.password = password
+        self.createdAt = createdAt
