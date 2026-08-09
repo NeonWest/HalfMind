37a7c7d9d8952d792bf48acf88c3ea3661e0b50d
Nihat-Hcyv <haciyevnihat73@gmail.com>
2026-08-10 02:44:35 +0400

    Initialize backend FastAPI application with Supabase integration and frontend ignore patterns

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,9 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+from supabase import create_client
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -7,3 +12,7 @@ def root():
     return("You Accessed To Root Page. Congrats!")
 
 
+supabase_url = os.getenv("SUPABASE_URL")
+supabase_key = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(supabase_url, supabase_key)
