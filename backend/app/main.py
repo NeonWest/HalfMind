4d61c3586aeb77c1fadf206a017d79bb2bb585cb
omaratakishi <omer.atakishiyev849@gmail.com>
2026-08-10 21:32:16 +0200

    Added SQLAlchemy user and note models

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,3 @@ def root():
     return("You Accessed To Root Page. Congrats!")
 
 
-supabase_url = os.getenv("SUPABASE_URL")
-supabase_key = os.getenv("SUPABASE_KEY")
-
-supabase = create_client(supabase_url, supabase_key)
