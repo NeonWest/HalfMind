bd59eb6963b3e0fba9e76b22444bdaa66c691c58
omaratakishi <omer.atakishiyev849@gmail.com>
2026-08-14 19:14:55 +0200

    Added responsive footer section

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,5 +1,6 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
+import Footer from "../components/Footer";
 import "./LandingPage.css"
 
 const LandingPage = () => {
@@ -7,6 +8,7 @@ const LandingPage = () => {
         <>
             <Navbar />
             <Hero />
+            <Footer />
 
 
 
